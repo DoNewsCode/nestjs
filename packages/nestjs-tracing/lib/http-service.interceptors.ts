bd47367d3b19f7cc84466fc8770db72f8bff53c0
李太新 <litaixin@donews.com>
2021-06-22 14:33:46 +0800

    fix: 修复tracing http-server interceptor在非http request中无法获取的错误

--- packages/nestjs-tracing/lib/http-service.interceptors.ts
+++ packages/nestjs-tracing/lib/http-service.interceptors.ts
@@ -11,55 +11,63 @@ import { TracingModule } from './tracing.module';
 export class HttpServiceInterceptors {
   static interceptRequest(): any {
     return [
-      (config: AxiosRequestConfig) => {
+      (config: AxiosRequestConfig): AxiosRequestConfig => {
         const tracer = TracingModule.tracer;
 
         const asyncContext = AsyncContext.getInstance();
-        let context = asyncContext.get(TRACER_CARRIER_INFO);
-        if (!context) {
-          context = {};
-          asyncContext.set(TRACER_CARRIER_INFO, context);
+        try {
+          let context = asyncContext.get(TRACER_CARRIER_INFO);
+          if (!context) {
+            context = {};
+            asyncContext.set(TRACER_CARRIER_INFO, context);
+          }
+
+          const ctx = tracer.extract(FORMAT_TEXT_MAP, context);
+
+          let span: Span;
+          if (ctx) {
+            span = tracer.startSpan('http', { childOf: ctx });
+          } else {
+            span = tracer.startSpan('http');
+          }
+          span.addTags({ url: config.url, method: config.method });
+
+          tracer.inject(span, FORMAT_HTTP_HEADERS, config.headers);
+
+          const spanConfig: any = config;
+          spanConfig.span = span;
+          return spanConfig as AxiosRequestConfig;
+        } catch (err) {
+          return config;
         }
-
-        const ctx = tracer.extract(FORMAT_TEXT_MAP, context);
-
-        let span: Span;
-        if (ctx) {
-          span = tracer.startSpan('http', { childOf: ctx });
-        } else {
-          span = tracer.startSpan('http');
-        }
-        span.addTags({ url: config.url, method: config.method });
-
-        tracer.inject(span, FORMAT_HTTP_HEADERS, config.headers);
-
-        const spanConfig: any = config;
-        spanConfig.span = span;
-        return spanConfig as AxiosRequestConfig;
       },
-      (error: any) => Promise.reject(error),
+      (error: any): Promise<any> => Promise.reject(error),
     ];
   }
 
   static interceptResponse(): any {
     return [
-      (response: AxiosResponse) => {
+      (response: AxiosResponse): any => {
         const config = response.config as AxiosRequestConfig & { span: Span };
-        config.span.log({
-          result: response.data,
-          statusCode: response.status,
-        });
-        config.span.finish();
+        if (config?.span) {
+          config.span.log({
+            result: response.data,
+            statusCode: response.status,
+          });
+          config.span.finish();
+        }
         return response;
       },
-      (error: any) => {
+      (error: any): Promise<any> => {
         const config = error.config as AxiosRequestConfig & { span: Span };
-        config.span.log({
-          result: error.response?.data,
-          statusCode: error.response?.status,
-        });
-        config.span.setTag(Tags.ERROR, true);
-        config.span.finish();
+        if (config?.span) {
+          config.span.log({
+            result: error.response?.data,
+            statusCode: error.response?.status,
+          });
+          config.span.setTag(Tags.ERROR, true);
+          config.span.finish();
+        }
         return Promise.reject(error);
       },
     ];
